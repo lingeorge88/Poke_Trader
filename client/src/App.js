dee01580c9c0b1e6cfe881244d42524ca4e47688
lingeorge88 <lingeorge04@gmail.com>
2023-05-23 21:39:47 -0700

    added a sample search bar with basic functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchPokemon = (search) => {
+    setSearchTerm(search);
+  }
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetch(`https://api.pokemontcg.io/v2/cards?q=name:${searchTerm}`)
+        .then(response => response.json())
+        .then(data => setCards(data.data))
+        .catch(error => console.error(error));
+    }
+  }, [searchTerm]);
+
   return (
     <div className="App">
-      <h1>Hello, Pokemon App!</h1>
+      <h1>Pokemon App</h1>
+      <SearchBar onSearch={searchPokemon} />
+      {cards && cards.map(card => (
+        <div key={card.id}>
+          <h2>{card.name}</h2>
+          <img src={card.images.small} alt={card.name} />
+        </div>
+      ))}
     </div>
   );
 }
