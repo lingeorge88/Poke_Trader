2d80e399a04ba0596212ef81f08e1d3a57c5fda6
Wendy Vu <wendyvuhoo@gmail.com>
2023-06-01 19:34:35 -0700

    updated server side with more backbone codes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,7 @@
 const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
 
 const resolvers = {
   Query: {
