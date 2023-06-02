f6d35d029e4abbdb2724c096664e5943b5fb5c0d
MC <119978958+mcbariekman@users.noreply.github.com>
2023-06-01 19:20:24 -0700

    function
    card trading works

--- trade functionality/trading.js
+++ trade functionality/trading.js
@@ -1,67 +1,52 @@
-const MAX_CARDS_PER_TRADE = 5; // Maximum number of cards allowed per trade
+// Player cards
+const player1Card = 'Charizard';
+const player2Card = 'Blastoise';
 
-// Route for handling trade requests
-app.post('/trade', (req, res) => {
-  const { player1Cards, player2Cards } = req.body;
+// Player 1 elements
+const player1CardElement = document.getElementById('player1Card');
+const player1ConfirmButton = document.getElementById('player1Confirm');
+const player1DenyButton = document.getElementById('player1Deny');
 
-  // Validate the trade request
-  if (player1Cards.length <= MAX_CARDS_PER_TRADE && player2Cards.length <= MAX_CARDS_PER_TRADE) {
-    // Trade request is valid, proceed with trade validation
-    // ...
-    res.status(200).json({ message: 'Trade request accepted' });
-  } else {
-    // Trade request does not meet the required conditions
-    res.status(400).json({ error: 'Invalid trade request' });
-  }
+// Player 2 elements
+const player2CardElement = document.getElementById('player2Card');
+const player2ConfirmButton = document.getElementById('player2Confirm');
+const player2DenyButton = document.getElementById('player2Deny');
+
+// Add event listeners for Player 1 buttons
+player1ConfirmButton.addEventListener('click', () => {
+  confirmTrade('player1');
 });
 
-// Validate the trade request
-function validateTrade(player1Cards, player2Cards) {
-  // Check if both players agree on the trade
-  const player1Agrees = true; // Confirmation - player 1
-  const player2Agrees = true; // Confirmation - player 2
-
-  // Check card ownership
-  const player1OwnsCards = checkCardOwnership(player1Cards, player1);
-  const player2OwnsCards = checkCardOwnership(player2Cards, player2);
-
-  // Return true if all conditions are met, false otherwise
-  return (
-    player1Agrees &&
-    player2Agrees &&
-    player1OwnsCards &&
-    player2OwnsCards
-  );
-}
-
-// Update card inventories
-function updateCardInventory(playerId, cardsToAdd, cardsToRemove) {
-  // Update the player's card inventory in the database
-  // Remove cardsToRemove from the player's inventory
-  // Add cardsToAdd to the player's inventory
-  // Ensure atomicity and consistency of the updates using transactions or other mechanisms
-}
-
-// Store trade record in the database
-function storeTradeRecord(player1, player2, player1Cards, player2Cards) {
-  const newTrade = new Trade({
-    player1,
-    player2,
-    player1Cards,
-    player2Cards,
-    status: 'completed', // Trade complete
-  });
-
-  newTrade.save(); // Save trade record
-}
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  // Handle trade-related errors and return appropriate responses
-  if (err instanceof TradeValidationError) {
-    res.status(400).json({ error: 'Invalid trade request' });
-  } else {
-    // Handle other errors
-    res.status(500).json({ error: 'Internal server error' });
-  }
+player1DenyButton.addEventListener('click', () => {
+  denyTrade();
 });
+
+// Add event listeners for Player 2 buttons
+player2ConfirmButton.addEventListener('click', () => {
+  confirmTrade('player2');
+});
+
+player2DenyButton.addEventListener('click', () => {
+  denyTrade();
+});
+
+// Function to confirm the trade
+function confirmTrade(player) {
+  if (player === 'player1') {
+    player1CardElement.textContent = player2Card;
+    player2CardElement.textContent = player1Card;
+  } else if (player === 'player2') {
+    player1CardElement.textContent = player2Card;
+    player2CardElement.textContent = player1Card;
+  }
+
+  alert('Trade confirmed!');
+}
+
+// Function to deny the trade
+function denyTrade() {
+  player1CardElement.textContent = player1Card;
+  player2CardElement.textContent = player2Card;
+
+  alert('Trade denied!');
+}
