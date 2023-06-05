d4a7ce1e480bd7dcca3a3b9fe3af66bf267118ba
lingeorge88 <lingeorge04@gmail.com>
2023-06-05 14:35:53 -0700

    updated server.js file and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,11 +5,68 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async () => {
-      return User.find({});
+    user: async (parent, { id }) => {
+      return User.findById(id);
     },
+    currentUser: async (parent, args, context) => {
+      if (context.user) {
+        return User.findById(context.user._id);
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    users: async () => {
+      return User.find();
+    }
   },
-//   Mutation: {},
+  
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveCard: async (parent, card, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedCards: card } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    removeCard: async (parent, { cardId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedCards: { cardId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  }
 };
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,44 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-// const db = require('./config/connection');
+
 // const routes = require('./routes');
+const { authMiddleWare } = require('./utils/auth');
+const {typeDefs, resolvers} = require('./schemas');
+const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// create a new instance of Apollo server and pass in our schema data and context
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+// express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());  
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  // Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+    
+    db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      })
+    })
+    };
+    
+  // Call the async function to start the server
+    startApolloServer(typeDefs, resolvers);
