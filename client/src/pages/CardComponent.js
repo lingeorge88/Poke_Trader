d0b10b440999ed604f4726edde1d25197b51df89
MC <119978958+mcbariekman@users.noreply.github.com>
2023-06-06 20:04:45 -0700

    card component / my collection
    adding in cardtrade

--- client/src/pages/CardComponent.js
+++ client/src/pages/CardComponent.js
@@ -3,6 +3,7 @@ import { Grid, Card, CardContent, Typography, IconButton, Box } from '@mui/mater
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import ZoomPopover from '../components/CardPopOver';
+import axios from 'axios';
 
 const CardComponent = ({ card, handleDelete, showDelete, handleSave }) => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -15,26 +16,37 @@ const CardComponent = ({ card, handleDelete, showDelete, handleSave }) => {
     setAnchorEl(null);
   };
 
+  const handleCardDelete = async (cardId) => {
+    try {
+      await axios.delete(`/api/cards/${cardId}`);
+      handleDelete(cardId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={card.id}>
       <Card>
         <CardContent>
-          <Typography variant="h5" style={{ marginBottom: '8px' }}>{card.name}</Typography>
+          <Typography variant="h5" style={{ marginBottom: '8px' }}>
+            {card.name}
+          </Typography>
           <img src={card.set.images.logo} alt={card.name} style={{ width: '65px', height: 'auto' }} />
           <img src={card.images.small} alt={card.name} />
           <Box display="flex" justifyContent="center" marginTop="8px">
-          <IconButton onClick={() => handleSave(card.id)}>
-            <CatchingPokemonIcon fontSize="large" />
-          </IconButton>
+            <IconButton onClick={() => handleSave(card.id)}>
+              <CatchingPokemonIcon fontSize="large" />
+            </IconButton>
             {showDelete && (
-              <IconButton aria-label="delete" onClick={() => handleDelete(card.id)}>
+              <IconButton aria-label="delete" onClick={() => handleCardDelete(card.id)}>
                 <DeleteOutlineIcon />
               </IconButton>
             )}
-            <ZoomPopover 
-              anchorEl={anchorEl} 
-              handleOpen={handleOpen} 
-              handleClose={handleClose} 
+            <ZoomPopover
+              anchorEl={anchorEl}
+              handleOpen={handleOpen}
+              handleClose={handleClose}
               cardSetName={card.set.name}
               cardSetSeries={card.set.series}
               releaseDate={card.set.releaseDate}
--- client/src/pages/MyCollection.js
+++ client/src/pages/MyCollection.js
@@ -4,24 +4,35 @@ import { Button, Grid } from '@mui/material';
 import Lottie from 'lottie-react';
 import Diglett from '../assets/diglettloading.json';
 import CardComponent from './CardComponent';
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_CURRENT_USER } from '../utils/queries';
-import { REMOVE_CARD } from '../utils/mutations';
+import axios from 'axios';
 
 const MyCollections = () => {
-  const { loading, data } = useQuery(QUERY_CURRENT_USER);
-  const savedCards = data?.currentUser?.savedCards  || [];
-  const [removeCard] = useMutation(REMOVE_CARD);
+  const [loading, setLoading] = useState(true);
+  const [savedCards, setSavedCards] = useState([]);
 
-  async function handleCardDelete(cardId){
+  useEffect(() => {
+    loadSavedCards();
+  }, []);
+
+  const loadSavedCards = async () => {
     try {
-        await removeCard({
-            variables: {cardId: cardId}
-        })
-    } catch (err) {
-        console.error(err);
+      const response = await axios.get('/api/cards'); // Replace '/api/cards' with your backend API route that retrieves saved cards
+      setSavedCards(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
     }
-  }
+  };
+
+  const handleCardDelete = async (cardId) => {
+    try {
+      await axios.delete(`/api/cards/${cardId}`); // Replace '/api/cards' with your backend API route for deleting a card
+      setSavedCards(savedCards.filter((card) => card._id !== cardId));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   if (loading) {
     return <Lottie animationData={Diglett} />;
@@ -31,17 +42,22 @@ const MyCollections = () => {
     <div className="App">
       <h1>My Collection</h1>
       <Grid container spacing={2} style={{ marginTop: '30px' }}>
-        {savedCards.map((card) => <CardComponent card={card} handleDelete={handleCardDelete} showDelete={true}/>)}
-        
+        {savedCards.map((card) => (
+          <CardComponent
+            card={card}
+            handleDelete={handleCardDelete}
+            showDelete={true}
+            key={card._id} // Assuming your card object has an '_id' field as its unique identifier
+          />
+        ))}
       </Grid>
       <div style={{ marginTop: '20px' }}>
-        <Button 
-          component={Link} to="/home">
+        <Button component={Link} to="/home">
           Home
         </Button>
       </div>
     </div>
   );
-}
+};
 
 export default MyCollections;
