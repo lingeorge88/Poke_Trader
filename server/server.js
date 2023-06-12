61cea2fe670f72b46d07af1ee6b4c48149d7f9a2
lingeorge88 <lingeorge04@gmail.com>
2023-06-12 14:02:47 -0700

    added back build folder to handle route errors

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
   }
 
   // Create a new instance of an Apollo server with the GraphQL schema
