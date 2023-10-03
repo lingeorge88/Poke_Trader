13f919e6f6242682350defc096b4d90a5c208215
lingeorge88 <lingeorge04@gmail.com>
2023-10-02 18:18:51 -0700

    added missing files

---
+++ client/src/components/Diglett.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import Diglett from '../assets/diglettloading.json';
+
+const DiglettAnimation = () => {
+
+      return  <Lottie animationData={Diglett} />
+};
+
+export default DiglettAnimation;
---
+++ client/src/components/Squirtle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import Squirtle from '../assets/squirtle.json';
+
+const SquirtleAnimation = () => {
+
+      return  <Lottie animationData={Squirtle} />
+};
+
+export default SquirtleAnimation;
